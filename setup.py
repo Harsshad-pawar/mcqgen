133ab7cc88b0b7b48292e8c1341c577298679b39
Harsshad-pawar <pawar.harshad186@gmail.com>
2024-01-21 17:17:46 +0400

    created logger

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
   name='mcqgenerator',
   version='0.1.0',
   author='Harsshad Pawar',
+  author_email='pawar.harshad186@gmail.com',
   install_requires=['openai','langchain','streamlit','python-dotenv','pyPDF2'],
         packages=find_packages()
   )
